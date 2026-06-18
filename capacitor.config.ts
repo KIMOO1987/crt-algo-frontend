81e1c8fe7d5141b8aa9b3a3a81f4581252e28deb
Kalim Ahmed <58071152+KIMOO1987@users.noreply.github.com>
2026-06-18 09:00:32 +0500

    Delete capacitor.config.ts

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.crtalgo.app',
-  appName: 'CRT-ALGO Pro',
-  webDir: 'out'
-};
-
-export default config;
